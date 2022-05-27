12816c9f6e0a247697a3aa75d36410ecebbb4e77
sunli829 <sunli829@users.noreply.github.com>
2022-05-27 04:35:45 +0000

    Automated deployment: Fri May 27 04:35:45 UTC 2022 94d64c915e827428103efd77ad93750ad621666d

---
+++ rust/0.2.2/implementors/num_enum/trait.FromPrimitive.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["longbridge"] = [{"text":"impl FromPrimitive for <a class=\"enum\" href=\"longbridge/quote/enum.TradeDirection.html\" title=\"enum longbridge::quote::TradeDirection\">TradeDirection</a>","synthetic":false,"types":["longbridge::quote::types::TradeDirection"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
