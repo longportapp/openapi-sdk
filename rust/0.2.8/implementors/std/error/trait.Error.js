97896e10c8fcb5f1b743c366cd2fee8bbc9f65c5
sunli829 <sunli829@users.noreply.github.com>
2022-06-01 12:29:05 +0000

    Automated deployment: Wed Jun  1 12:29:05 UTC 2022 ca979814bd04dcc32dc48f15b164b66c60f47d2d

---
+++ rust/0.2.8/implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["longbridge"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"longbridge/enum.Error.html\" title=\"enum longbridge::Error\">Error</a>","synthetic":false,"types":["longbridge::error::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
