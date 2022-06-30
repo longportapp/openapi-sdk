c3516638c7c0870412de89d429d489ca6d798fbe
sunli829 <sunli829@users.noreply.github.com>
2022-06-30 01:47:43 +0000

    Automated deployment: Thu Jun 30 01:47:43 UTC 2022 1faf4f6ea641b28050292eb0cadf2546c838d3e7

--- rust/0.2.23/implementors/core/ops/bit/trait.BitXorAssign.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["longbridge"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/ops/bit/trait.BitXorAssign.html\" title=\"trait core::ops::bit::BitXorAssign\">BitXorAssign</a>&lt;<a class=\"struct\" href=\"longbridge/quote/struct.SubFlags.html\" title=\"struct longbridge::quote::SubFlags\">SubFlags</a>&gt; for <a class=\"struct\" href=\"longbridge/quote/struct.SubFlags.html\" title=\"struct longbridge::quote::SubFlags\">SubFlags</a>","synthetic":false,"types":["longbridge::quote::sub_flags::SubFlags"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
