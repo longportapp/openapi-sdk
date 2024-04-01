ed4d92ab34322aac997a4b07490f5d07f8560a1a
sunli829 <sunli829@users.noreply.github.com>
2024-04-01 07:20:07 +0000

    Automated deployment: Mon Apr  1 07:20:07 UTC 2024 1b46b01b026cdd5a2b1c7f699dc7fc032e9fcc67

--- rust/trait.impl/bitflags/traits/trait.Flags.js
+++ rust/trait.impl/bitflags/traits/trait.Flags.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"longport":[["impl Flags for <a class=\"struct\" href=\"longport/quote/struct.DerivativeType.html\" title=\"struct longport::quote::DerivativeType\">DerivativeType</a>"],["impl Flags for <a class=\"struct\" href=\"longport/quote/struct.SubFlags.html\" title=\"struct longport::quote::SubFlags\">SubFlags</a>"]]
+"longport":[["impl Flags for <a class=\"struct\" href=\"longport/quote/struct.SubFlags.html\" title=\"struct longport::quote::SubFlags\">SubFlags</a>"],["impl Flags for <a class=\"struct\" href=\"longport/quote/struct.DerivativeType.html\" title=\"struct longport::quote::DerivativeType\">DerivativeType</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
