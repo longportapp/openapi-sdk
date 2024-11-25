7824911b8c383bb46f5fa7246ae944a2a653dcd3
Sunli <scott_s829@163.com>
2024-11-26 00:15:48 +0800

    update js example

--- examples/nodejs/subscribe_candlesticks.js
+++ examples/nodejs/subscribe_candlesticks.js
@@ -1,7 +1,9 @@
 const { Config, QuoteContext, Period } = require("longport");
 
 let config = Config.fromEnv();
+let globalCtx;
 QuoteContext.new(config).then((ctx) => {
+  globalCtx = ctx; // keep a reference to the context, so it doesn't get garbage collected
   ctx.setOnCandlestick((_, event) => console.log(event.toString()));
   ctx.subscribeCandlesticks("AAPL.US", Period.Min_1);
 });
--- examples/nodejs/subscribe_quote.js
+++ examples/nodejs/subscribe_quote.js
@@ -1,11 +1,9 @@
 const { Config, QuoteContext, SubType } = require("longport");
 
 let config = Config.fromEnv();
+let globalCtx;
 QuoteContext.new(config).then((ctx) => {
+  globalCtx = ctx; // keep a reference to the context, so it doesn't get garbage collected
   ctx.setOnQuote((_, event) => console.log(event.toString()));
-  ctx.subscribe(
-    ["700.HK", "AAPL.US", "TSLA.US", "NFLX.US"],
-    [SubType.Quote],
-    true
-  );
+  ctx.subscribe(["TSLA.US"], [SubType.Quote], true);
 });
