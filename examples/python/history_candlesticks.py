fdef2a97d79f0eac04ee32d1e4fb644bf5294611
Sunli <scott_s829@163.com>
2025-01-14 10:44:49 +0800

    update python examples

--- examples/python/history_candlesticks.py
+++ examples/python/history_candlesticks.py
@@ -7,13 +7,15 @@ ctx = QuoteContext(config)
 # get candlesticks by offset
 print("get candlesticks by offset")
 print("====================")
-candlesticks = ctx.history_candlesticks_by_offset("700.HK", Period.Day, AdjustType.NoAdjust, False, datetime.datetime(2023, 8, 18), 10)
+candlesticks = ctx.history_candlesticks_by_offset(
+    "700.HK", Period.Day, AdjustType.NoAdjust, False, 10, datetime.datetime(2023, 8, 18))
 for candlestick in candlesticks:
     print(candlestick)
 
 # get candlesticks by date
 print("get candlesticks by date")
 print("====================")
-candlesticks = ctx.history_candlesticks_by_date("700.HK", Period.Day, AdjustType.NoAdjust, datetime.date(2022, 5, 5), datetime.date(2022, 6, 23))
+candlesticks = ctx.history_candlesticks_by_date(
+    "700.HK", Period.Day, AdjustType.NoAdjust, datetime.date(2022, 5, 5), datetime.date(2022, 6, 23))
 for candlestick in candlesticks:
     print(candlestick)
