83c2e40442de8cfeeb13a23e204875fae8d02282
sunli829 <sunli829@users.noreply.github.com>
2025-02-03 03:09:08 +0000

    Automated deployment: Mon Feb  3 03:09:08 UTC 2025 466a91f414ed11dea70acde07d1a633161936651

--- v2/rust/trait.impl/core/fmt/trait.UpperHex.js
+++ v2/rust/trait.impl/core/fmt/trait.UpperHex.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["longport",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"longport/quote/struct.DerivativeType.html\" title=\"struct longport::quote::DerivativeType\">DerivativeType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"longport/quote/struct.SubFlags.html\" title=\"struct longport::quote::SubFlags\">SubFlags</a>"]]]]);
+    var implementors = Object.fromEntries([["longport",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"longport/quote/struct.DerivativeType.html\" title=\"struct longport::quote::DerivativeType\">DerivativeType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"longport/quote/struct.SubFlags.html\" title=\"struct longport::quote::SubFlags\">SubFlags</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
---
+++ v2/rust/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["longport",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"longport/quote/struct.QuoteContext.html\" title=\"struct longport::quote::QuoteContext\">QuoteContext</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"longport/trade/struct.TradeContext.html\" title=\"struct longport::trade::TradeContext\">TradeContext</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[582]}
