5f7a0f8f3cfbc5c2debe3da454cb4221a338840d
sunli829 <sunli829@users.noreply.github.com>
2025-02-25 07:03:40 +0000

    Automated deployment: Tue Feb 25 07:03:40 UTC 2025 f0bc77032455b039c0532685fe6ab62aa13133e5

--- v2/rust/trait.impl/core/error/trait.Error.js
+++ v2/rust/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["longport",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"longport/enum.Error.html\" title=\"enum longport::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"longport/enum.SimpleError.html\" title=\"enum longport::SimpleError\">SimpleError</a>"]]]]);
+    var implementors = Object.fromEntries([["longport",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"longport/enum.Error.html\" title=\"enum longport::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"longport/enum.SimpleError.html\" title=\"enum longport::SimpleError\">SimpleError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
